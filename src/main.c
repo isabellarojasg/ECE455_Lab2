6d4fe38d60835a9d7cd0f6d1096cc1eb608b2115
Isabella Rojas <68630621+isabellarojasg@users.noreply.github.com>
2023-03-09 17:04:10 -0800

    Updating main

--- src/main.c
+++ src/main.c
@@ -1,27 +1,21 @@
 /*
     FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
     All rights reserved
-
     VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
-
     This file is part of the FreeRTOS distribution.
-
     FreeRTOS is free software; you can redistribute it and/or modify it under
     the terms of the GNU General Public License (version 2) as published by the
     Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
-
     ***************************************************************************
     >>!   NOTE: The modification to the GPL is included to allow you to     !<<
     >>!   distribute a combined work that includes FreeRTOS without being   !<<
     >>!   obliged to provide the source code for proprietary components     !<<
     >>!   outside of the FreeRTOS kernel.                                   !<<
     ***************************************************************************
-
     FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
     WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
     FOR A PARTICULAR PURPOSE.  Full license text is available on the following
     link: http://www.freertos.org/a00114.html
-
     ***************************************************************************
      *                                                                       *
      *    FreeRTOS provides completely free yet professionally developed,    *
@@ -35,35 +29,27 @@
      *    http://www.FreeRTOS.org/Documentation                              *
      *                                                                       *
     ***************************************************************************
-
     http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
     the FAQ page "My application does not run, what could be wwrong?".  Have you
     defined configASSERT()?
-
     http://www.FreeRTOS.org/support - In return for receiving this top quality
     embedded software for free we request you assist our global community by
     participating in the support forum.
-
     http://www.FreeRTOS.org/training - Investing in training allows your team to
     be as productive as possible as early as possible.  Now you can receive
     FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
     Ltd, and the world's leading authority on the world's leading RTOS.
-
     http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
     including FreeRTOS+Trace - an indispensable productivity tool, a DOS
     compatible FAT file system, and our tiny thread aware UDP/IP stack.
-
     http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
     Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
-
     http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
     Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
     licenses offer ticketed support, indemnification and commercial middleware.
-
     http://www.SafeRTOS.com - High Integrity Systems also provide a safety
     engineered and independently SIL3 certified version for use in safety and
     mission critical applications that require provable dependability.
-
     1 tab == 4 spaces!
 */
 
@@ -73,22 +59,17 @@ FreeRTOS is a market leading RTOS from Real Time Engineers Ltd. that supports
 developed, strictly quality controlled, robust, supported, and free to use in
 commercial products without any requirement to expose your proprietary source
 code.
-
 This simple FreeRTOS demo does not make use of any IO ports, so will execute on
 any Cortex-M3 of Cortex-M4 hardware.  Look for TODO markers in the code for
 locations that may require tailoring to, for example, include a manufacturer
 specific header file.
-
 This is a starter project, so only a subset of the RTOS features are
 demonstrated.  Ample source comments are provided, along with web links to
 relevant pages on the http://www.FreeRTOS.org site.
-
 Here is a description of the project's functionality:
-
 The main() Function:
 main() creates the tasks and software timers described in this section, before
 starting the scheduler.
-
 The Queue Send Task:
 The queue send task is implemented by the prvQueueSendTask() function.
 The task uses the FreeRTOS vTaskDelayUntil() and xQueueSend() API functions to
@@ -96,7 +77,6 @@ periodically send the number 100 on a queue.  The period is set to 200ms.  See
 the comments in the function for more details.
 http://www.freertos.org/vtaskdelayuntil.html
 http://www.freertos.org/a00117.html
-
 The Queue Receive Task:
 The queue receive task is implemented by the prvQueueReceiveTask() function.
 The task uses the FreeRTOS xQueueReceive() API function to receive values from
@@ -105,30 +85,25 @@ receive task increments the ulCountOfItemsReceivedOnQueue variable each time it
 receives the value 100.  Therefore, as values are sent to the queue every 200ms,
 the value of ulCountOfItemsReceivedOnQueue will increase by 5 every second.
 http://www.freertos.org/a00118.html
-
 An example software timer:
 A software timer is created with an auto reloading period of 1000ms.  The
 timer's callback function increments the ulCountOfTimerCallbackExecutions
 variable each time it is called.  Therefore the value of
 ulCountOfTimerCallbackExecutions will count seconds.
 http://www.freertos.org/RTOS-software-timer.html
-
 The FreeRTOS RTOS tick hook (or callback) function:
 The tick hook function executes in the context of the FreeRTOS tick interrupt.
 The function 'gives' a semaphore every 500th time it executes.  The semaphore
 is used to synchronise with the event semaphore task, which is described next.
-
 The event semaphore task:
 The event semaphore task uses the FreeRTOS xSemaphoreTake() API function to
 wait for the semaphore that is given by the RTOS tick hook function.  The task
 increments the ulCountOfReceivedSemaphores variable each time the semaphore is
 received.  As the semaphore is given every 500ms (assuming a tick frequency of
 1KHz), the value of ulCountOfReceivedSemaphores will increase by 2 each second.
-
 The idle hook (or callback) function:
 The idle hook function queries the amount of free FreeRTOS heap space available.
 See vApplicationIdleHook().
-
 The malloc failed and stack overflow hook (or callback) functions:
 These two hook functions are provided as examples, but do not contain any
 functionality.
@@ -137,6 +112,8 @@ functionality.
 /* Standard includes. */
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "stm32f4_discovery.h"
 /* Kernel includes. */
 #include "stm32f4xx.h"
@@ -149,18 +126,7 @@ functionality.
 
 
 /*-----------------------------------------------------------*/
-#define mainQUEUE_LENGTH 100
-
-#define amber  	0
-#define green  	1
-#define red  	2
-#define blue  	3
-
-#define amber_led	LED3
-#define green_led	LED4
-#define red_led		LED5
-#define blue_led	LED6
-
+#define mainQUEUE_LENGTH 1
 
 /*
  * TODO: Implement this function for any hardware specific clock configuration
@@ -172,14 +138,34 @@ static void prvSetupHardware( void );
  * The queue send and receive tasks as described in the comments at the top of
  * this file.
  */
-static void Manager_Task( void *pvParameters );
-static void Blue_LED_Controller_Task( void *pvParameters );
-static void Green_LED_Controller_Task( void *pvParameters );
-static void Red_LED_Controller_Task( void *pvParameters );
-static void Amber_LED_Controller_Task( void *pvParameters );
+static void dds_task(void *pvParameters);
+static void generator_task(void *pvParameters);
+static void monitor_task(void *pvParameters);
+static void usd_task(void *pvParameters);
+static void task1_timer_callback(TimerHandle_t xTimer);
 
-xQueueHandle xQueue_handle = 0;
+QueueHandle_t xQueue_New_Tasks;
+QueueHandle_t xQueue_Completed_Tasks;
+QueueHandle_t xQueue_List_Requests;
+QueueHandle_t xQueue_List_Response;
 
+TimerHandle_t xTimer_Task1_Generator;
+
+enum task_type {PERIODIC, APERIODIC};
+
+struct dd_task {
+	TaskHandle_t t_handle;
+	enum task_type type;	//is this redeclaring task_type or?
+	uint32_t task_id;
+	uint32_t release_time;
+	uint32_t absolute_deadline;
+	uint32_t completion_time;
+};
+
+struct dd_task_list {
+	struct dd_task task;	//again, should have struct here??
+	struct dd_task_list *next_task;
+};
 
 /*-----------------------------------------------------------*/
 
@@ -187,10 +173,6 @@ int main(void)
 {
 
 	/* Initialize LEDs */
-	STM_EVAL_LEDInit(amber_led);
-	STM_EVAL_LEDInit(green_led);
-	STM_EVAL_LEDInit(red_led);
-	STM_EVAL_LEDInit(blue_led);
 
 	/* Configure the system ready to run the demo.  The clock configuration
 	can be done here if it was not done before main() was called. */
@@ -199,17 +181,33 @@ int main(void)
 
 	/* Create the queue used by the queue send and queue receive tasks.
 	http://www.freertos.org/a00116.html */
-	xQueue_handle = xQueueCreate( 	mainQUEUE_LENGTH,		/* The number of items the queue can hold. */
-							sizeof( uint16_t ) );	/* The size of each item the queue holds. */
+	xQueue_New_Tasks = xQueueCreate(mainQUEUE_LENGTH,	/* The number of items the queue can hold. */
+									sizeof( uint16_t ) );	/* The size of each item the queue holds. */
+	xQueue_Completed_Tasks = xQueueCreate(mainQUEUE_LENGTH,	/* The number of items the queue can hold. */
+										sizeof( uint16_t ) );	/* The size of each item the queue holds. */
+	xQueue_List_Requests = xQueueCreate(mainQUEUE_LENGTH,	/* The number of items the queue can hold. */
+											sizeof( uint16_t ) );	/* The size of each item the queue holds. */
+	xQueue_List_Response = xQueueCreate(mainQUEUE_LENGTH,	/* The number of items the queue can hold. */
+												sizeof( uint16_t ) );	/* The size of each item the queue holds. */
 
 	/* Add to the registry, for the benefit of kernel aware debugging. */
-	vQueueAddToRegistry( xQueue_handle, "MainQueue" );
+	vQueueAddToRegistry( xQueue_New_Tasks, "New Tasks Queue" );	//holds new tasks created by generator for the DDS to schedule
+	vQueueAddToRegistry( xQueue_Completed_Tasks, "Completed Tasks Queue" );	//holds tasks that have been completed for the DDS to move out of the active list
+	vQueueAddToRegistry( xQueue_List_Requests, "List Request Queue" );	//holds requests for copies of the current states of the DDS task lists
+	vQueueAddToRegistry( xQueue_List_Response, "List Response Queue" );	//holds copies of the current states of the DDS task lists
 
-	xTaskCreate( Manager_Task, "Manager", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-	xTaskCreate( Blue_LED_Controller_Task, "Blue_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate( Red_LED_Controller_Task, "Red_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate( Green_LED_Controller_Task, "Green_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate( Amber_LED_Controller_Task, "Amber_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
+	xTimer_Task1_Generator = xTimerCreate("Task1 Generation Timer", 100, pdFALSE, 0, task1_timer_callback);
+
+	xTaskCreate( dds_task, "Deadline Drive Scheduler", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate( generator_task, "Deadline-Driven Task Generator", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+	//xTaskCreate( monitor_task, "Monitor Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	//xTaskCreate( complete_dd_task, "Complete Deadline Driven Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	//xTaskCreate( release_dd_task, "Release Deadline Driven Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	//xTaskCreate( get_active_dd_task_list, "Deadline Drive Scheduler", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	//xTaskCreate( get_completed_task_list, "Deadline Drive Scheduler", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
+
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
@@ -217,161 +215,41 @@ int main(void)
 	return 0;
 }
 
-
 /*-----------------------------------------------------------*/
 
-static void Manager_Task( void *pvParameters )
-{
-	uint16_t tx_data = amber;
+static void dds_task(void *pvParameters){
 
-
-	while(1)
-	{
-
-		if(tx_data == amber)
-			STM_EVAL_LEDOn(amber_led);
-		if(tx_data == green)
-			STM_EVAL_LEDOn(green_led);
-		if(tx_data == red)
-			STM_EVAL_LEDOn(red_led);
-		if(tx_data == blue)
-			STM_EVAL_LEDOn(blue_led);
-
-		if( xQueueSend(xQueue_handle,&tx_data,1000))
-		{
-			printf("Manager: %u ON!\n", tx_data);
-			if(++tx_data == 4)
-				tx_data = 0;
-			vTaskDelay(1000);
-		}
-		else
-		{
-			printf("Manager Failed!\n");
-		}
-	}
 }
 
-/*-----------------------------------------------------------*/
+static void task1_timer_callback(TimerHandle_t xTimer){
 
-static void Blue_LED_Controller_Task( void *pvParameters )
-{
-	uint16_t rx_data;
-	while(1)
-	{
-		if(xQueueReceive(xQueue_handle, &rx_data, 500))
-		{
-			if(rx_data == blue)
-			{
-				vTaskDelay(250);
-				STM_EVAL_LEDOff(blue_led);
-				printf("Blue Off.\n");
-			}
-			else
-			{
-				if( xQueueSend(xQueue_handle,&rx_data,1000))
-					{
-						printf("BlueTask GRP (%u).\n", rx_data); // Got wwrong Package
-						vTaskDelay(500);
-					}
-			}
-		}
-	}
 }
 
-
-/*-----------------------------------------------------------*/
-
-static void Green_LED_Controller_Task( void *pvParameters )
-{
-	uint16_t rx_data;
-	while(1)
-	{
-		if(xQueueReceive(xQueue_handle, &rx_data, 500))
-		{
-			if(rx_data == green)
-			{
-				vTaskDelay(250);
-				STM_EVAL_LEDOff(green_led);
-				printf("Green Off.\n");
-			}
-			else
-			{
-				if( xQueueSend(xQueue_handle,&rx_data,1000))
-					{
-						printf("GreenTask GRP (%u).\n", rx_data); // Got wrong Package
-						vTaskDelay(500);
-					}
-			}
-		}
-	}
+static void generator_task(void *pvParameters){
+	struct dd_task new_task;
+	TaskHandle_t *usd_task_handle;
+	xTaskCreate( usd_task, "User-Defined Task", configMINIMAL_STACK_SIZE, NULL, 1, usd_task_handle);
+	new_task.t_handle = *usd_task_handle;
+	//release_dd_task();
+	vTaskSuspend();
 }
 
-/*-----------------------------------------------------------*/
+static void monitor_task(void *pvParameters){
 
-static void Red_LED_Controller_Task( void *pvParameters )
-{
-	uint16_t rx_data;
-	while(1)
-	{
-		if(xQueueReceive(xQueue_handle, &rx_data, 500))
-		{
-			if(rx_data == red)
-			{
-				vTaskDelay(250);
-				STM_EVAL_LEDOff(red_led);
-				printf("Red off.\n");
-			}
-			else
-			{
-				if( xQueueSend(xQueue_handle,&rx_data,1000))
-					{
-						printf("RedTask GRP (%u).\n", rx_data); // Got wrong Package
-						vTaskDelay(500);
-					}
-			}
-		}
-	}
 }
 
+static void usd_task(void *pvParameters){
 
-/*-----------------------------------------------------------*/
-
-static void Amber_LED_Controller_Task( void *pvParameters )
-{
-	uint16_t rx_data;
-	while(1)
-	{
-		if(xQueueReceive(xQueue_handle, &rx_data, 500))
-		{
-			if(rx_data == amber)
-			{
-				vTaskDelay(250);
-				STM_EVAL_LEDOff(amber_led);
-				printf("Amber Off.\n");
-			}
-			else
-			{
-				if( xQueueSend(xQueue_handle,&rx_data,1000))
-					{
-						printf("AmberTask GRP (%u).\n", rx_data); // Got wrong Package
-						vTaskDelay(500);
-					}
-			}
-		}
-	}
 }
-
-
 /*-----------------------------------------------------------*/
 
 void vApplicationMallocFailedHook( void )
 {
 	/* The malloc failed hook is enabled by setting
-	configUSE_MALLOC_FAILED_HOOK to 1 in FreeRTOSConfig.h.
-
+	   configUSE_MALLOC_FAILED_HOOK to 1 in FreeRTOSConfig.h.
 	Called if a call to pvPortMalloc() fails because there is insufficient
 	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
-	internally by FreeRTOS API functions that create tasks, queues, software 
+	internally by FreeRTOS API functions that create tasks, queues, software
 	timers, and semaphores.  The size of the FreeRTOS heap is set by the
 	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
 	for( ;; );
@@ -398,7 +276,6 @@ volatile size_t xFreeStackSpace;
 
 	/* The idle task hook is enabled by setting configUSE_IDLE_HOOK to 1 in
 	FreeRTOSConfig.h.
-
 	This function is called on each cycle of the idle task.  In this case it
 	does nothing useful, other than report the amount of FreeRTOS heap that
 	remains unallocated. */
@@ -406,10 +283,10 @@ volatile size_t xFreeStackSpace;
 
 	if( xFreeStackSpace > 100 )
 	{
-		/* By now, the kernel has allocated everything it is going to, so
-		if there is a lot of heap remaining unallocated then
-		the value of configTOTAL_HEAP_SIZE in FreeRTOSConfig.h can be
-		reduced accordingly. */
+			/* By now, the kernel has allocated everything it is going to, so
+			if there is a lot of heap remaining unallocated then
+			the value of configTOTAL_HEAP_SIZE in FreeRTOSConfig.h can be
+			reduced accordingly. */
 	}
 }
 /*-----------------------------------------------------------*/
@@ -423,4 +300,3 @@ static void prvSetupHardware( void )
 	/* TODO: Setup the clocks, etc. here, if they were not configured before
 	main() was called. */
 }
-
