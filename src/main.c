2a4738f611166aabd7403dd5e9eebc7531fe5d67
Kali Erickson <71956851+k-kali@users.noreply.github.com>
2023-03-09 17:20:28 -0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,21 +1,27 @@
 /*
     FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
     All rights reserved
+
     VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+
     This file is part of the FreeRTOS distribution.
+
     FreeRTOS is free software; you can redistribute it and/or modify it under
     the terms of the GNU General Public License (version 2) as published by the
     Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
+
     ***************************************************************************
     >>!   NOTE: The modification to the GPL is included to allow you to     !<<
     >>!   distribute a combined work that includes FreeRTOS without being   !<<
     >>!   obliged to provide the source code for proprietary components     !<<
     >>!   outside of the FreeRTOS kernel.                                   !<<
     ***************************************************************************
+
     FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
     WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
     FOR A PARTICULAR PURPOSE.  Full license text is available on the following
     link: http://www.freertos.org/a00114.html
+
     ***************************************************************************
      *                                                                       *
      *    FreeRTOS provides completely free yet professionally developed,    *
@@ -29,27 +35,35 @@
      *    http://www.FreeRTOS.org/Documentation                              *
      *                                                                       *
     ***************************************************************************
+
     http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
     the FAQ page "My application does not run, what could be wwrong?".  Have you
     defined configASSERT()?
+
     http://www.FreeRTOS.org/support - In return for receiving this top quality
     embedded software for free we request you assist our global community by
     participating in the support forum.
+
     http://www.FreeRTOS.org/training - Investing in training allows your team to
     be as productive as possible as early as possible.  Now you can receive
     FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
     Ltd, and the world's leading authority on the world's leading RTOS.
+
     http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
     including FreeRTOS+Trace - an indispensable productivity tool, a DOS
     compatible FAT file system, and our tiny thread aware UDP/IP stack.
+
     http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
     Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
+
     http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
     Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
     licenses offer ticketed support, indemnification and commercial middleware.
+
     http://www.SafeRTOS.com - High Integrity Systems also provide a safety
     engineered and independently SIL3 certified version for use in safety and
     mission critical applications that require provable dependability.
+
     1 tab == 4 spaces!
 */
 
@@ -59,17 +73,22 @@ FreeRTOS is a market leading RTOS from Real Time Engineers Ltd. that supports
 developed, strictly quality controlled, robust, supported, and free to use in
 commercial products without any requirement to expose your proprietary source
 code.
+
 This simple FreeRTOS demo does not make use of any IO ports, so will execute on
 any Cortex-M3 of Cortex-M4 hardware.  Look for TODO markers in the code for
 locations that may require tailoring to, for example, include a manufacturer
 specific header file.
+
 This is a starter project, so only a subset of the RTOS features are
 demonstrated.  Ample source comments are provided, along with web links to
 relevant pages on the http://www.FreeRTOS.org site.
+
 Here is a description of the project's functionality:
+
 The main() Function:
 main() creates the tasks and software timers described in this section, before
 starting the scheduler.
+
 The Queue Send Task:
 The queue send task is implemented by the prvQueueSendTask() function.
 The task uses the FreeRTOS vTaskDelayUntil() and xQueueSend() API functions to
@@ -77,6 +96,7 @@ periodically send the number 100 on a queue.  The period is set to 200ms.  See
 the comments in the function for more details.
 http://www.freertos.org/vtaskdelayuntil.html
 http://www.freertos.org/a00117.html
+
 The Queue Receive Task:
 The queue receive task is implemented by the prvQueueReceiveTask() function.
 The task uses the FreeRTOS xQueueReceive() API function to receive values from
@@ -85,25 +105,30 @@ receive task increments the ulCountOfItemsReceivedOnQueue variable each time it
 receives the value 100.  Therefore, as values are sent to the queue every 200ms,
 the value of ulCountOfItemsReceivedOnQueue will increase by 5 every second.
 http://www.freertos.org/a00118.html
+
 An example software timer:
 A software timer is created with an auto reloading period of 1000ms.  The
 timer's callback function increments the ulCountOfTimerCallbackExecutions
 variable each time it is called.  Therefore the value of
 ulCountOfTimerCallbackExecutions will count seconds.
 http://www.freertos.org/RTOS-software-timer.html
+
 The FreeRTOS RTOS tick hook (or callback) function:
 The tick hook function executes in the context of the FreeRTOS tick interrupt.
 The function 'gives' a semaphore every 500th time it executes.  The semaphore
 is used to synchronise with the event semaphore task, which is described next.
+
 The event semaphore task:
 The event semaphore task uses the FreeRTOS xSemaphoreTake() API function to
 wait for the semaphore that is given by the RTOS tick hook function.  The task
 increments the ulCountOfReceivedSemaphores variable each time the semaphore is
 received.  As the semaphore is given every 500ms (assuming a tick frequency of
 1KHz), the value of ulCountOfReceivedSemaphores will increase by 2 each second.
+
 The idle hook (or callback) function:
 The idle hook function queries the amount of free FreeRTOS heap space available.
 See vApplicationIdleHook().
+
 The malloc failed and stack overflow hook (or callback) functions:
 These two hook functions are provided as examples, but do not contain any
 functionality.
@@ -143,6 +168,8 @@ static void generator_task(void *pvParameters);
 static void monitor_task(void *pvParameters);
 static void usd_task(void *pvParameters);
 static void task1_timer_callback(TimerHandle_t xTimer);
+
+TaskHandle_t generator_handle;
 
 QueueHandle_t xQueue_New_Tasks;
 QueueHandle_t xQueue_Completed_Tasks;
@@ -196,11 +223,11 @@ int main(void)
 	vQueueAddToRegistry( xQueue_List_Requests, "List Request Queue" );	//holds requests for copies of the current states of the DDS task lists
 	vQueueAddToRegistry( xQueue_List_Response, "List Response Queue" );	//holds copies of the current states of the DDS task lists
 
-
+	//TODO:set period of timer
 	xTimer_Task1_Generator = xTimerCreate("Task1 Generation Timer", 100, pdFALSE, 0, task1_timer_callback);
 
 	xTaskCreate( dds_task, "Deadline Drive Scheduler", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-	xTaskCreate( generator_task, "Deadline-Driven Task Generator", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+	xTaskCreate( generator_task, "Deadline-Driven Task Generator", configMINIMAL_STACK_SIZE, NULL, 3, generator_handle);
 	//xTaskCreate( monitor_task, "Monitor Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 	//xTaskCreate( complete_dd_task, "Complete Deadline Driven Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 	//xTaskCreate( release_dd_task, "Release Deadline Driven Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
@@ -217,12 +244,17 @@ int main(void)
 
 /*-----------------------------------------------------------*/
 
+void release_dd_task(){
+	//xQueueSend();
+}
+
 static void dds_task(void *pvParameters){
 
 }
 
 static void task1_timer_callback(TimerHandle_t xTimer){
-
+	BaseType_t xYieldRequired = xTaskResumeFromISR(generator_handle);
+	portYIELD_FROM_ISR(xYieldRequired);
 }
 
 static void generator_task(void *pvParameters){
@@ -230,8 +262,8 @@ static void generator_task(void *pvParameters){
 	TaskHandle_t *usd_task_handle;
 	xTaskCreate( usd_task, "User-Defined Task", configMINIMAL_STACK_SIZE, NULL, 1, usd_task_handle);
 	new_task.t_handle = *usd_task_handle;
-	//release_dd_task();
-	vTaskSuspend();
+	release_dd_task();
+	vTaskSuspend(NULL);
 }
 
 static void monitor_task(void *pvParameters){
@@ -247,6 +279,7 @@ void vApplicationMallocFailedHook( void )
 {
 	/* The malloc failed hook is enabled by setting
 	   configUSE_MALLOC_FAILED_HOOK to 1 in FreeRTOSConfig.h.
+
 	Called if a call to pvPortMalloc() fails because there is insufficient
 	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
 	internally by FreeRTOS API functions that create tasks, queues, software
@@ -276,6 +309,7 @@ volatile size_t xFreeStackSpace;
 
 	/* The idle task hook is enabled by setting configUSE_IDLE_HOOK to 1 in
 	FreeRTOSConfig.h.
+
 	This function is called on each cycle of the idle task.  In this case it
 	does nothing useful, other than report the amount of FreeRTOS heap that
 	remains unallocated. */
